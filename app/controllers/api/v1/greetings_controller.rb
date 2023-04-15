9ef504f798b66ed08635d8df5a3771a98c1ca7ec
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-15 18:38:52 +0100

    Remove empty line

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,8 +1,6 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     greeting = Greeting.order('RANDOM()').first
-    response.headers['Access-Control-Allow-Origin'] = '*'
     render json: greeting
   end
-
 end
