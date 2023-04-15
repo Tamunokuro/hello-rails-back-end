a692d85628516cf1dd00d2640ec7af4aefebf3ad
Tamunokuro <tamunokrobluejack@gmail.com>
2023-04-15 18:29:18 +0100

    Fix linters

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,8 +1,7 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
-    greeting =  greeting = Greeting.find_by(id: rand(1..Greeting.count))
+    greeting = greeting = Greeting.find_by(id: rand(1..Greeting.count))
     # response.headers['Access-Control-Allow-Origin'] = '*'
     render json: greeting
   end
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::API
-    def cors_preflight_check
-        headers['Access-Control-Allow-Origin'] = '*'  # Replace with your desired origin(s)
-        headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
-        headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization, X-Requested-With, X-HTTP-Method-Override, Accept, access-control-allow-origin'
-        head :ok
-      end
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*' # Replace with your desired origin(s)
+    headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] =
+      'Content-Type, Authorization, X-Requested-With, X-HTTP-Method-Override, Accept, access-control-allow-origin'
+    head :ok
+  end
 end
